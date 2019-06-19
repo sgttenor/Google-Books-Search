3c9f570a4cd4b814c14c40ca11d60d3573ca86d1
Marco Teti <sgttenore.gmail.com>
2019-06-19 14:12:48 -0700

    installed nodemon and fixed errors

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const routes = require("./routes");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
 // Express middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(logger("dev"));
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
